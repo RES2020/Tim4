b20921d5f2c4e5a33d6aca8d68121792427a094d
Milenko98 <61869042+Milenko98@users.noreply.github.com>
2020-05-18 20:42:03 +0200

    Funkcija za unos teksta

---
+++ ResProjekat/UnosTeksta/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnosTeksta
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                UnesiteTekst ut = new UnesiteTekst();
+                ut.Unos();
+                Console.ReadLine();
+            }
+        }
+    }
+}
