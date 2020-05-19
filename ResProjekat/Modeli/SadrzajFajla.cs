36ef8367ae6c3b3e9ac12b0e1c708dfa499cde7b
Jovan275 <jovanconti86@gmail.com>
2020-05-19 12:33:23 +0200

    Klasa sadrzaj fajla napravljena

--- ResProjekat/Modeli/SadrzajFajla.cs
+++ ResProjekat/Modeli/SadrzajFajla.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace Modeli
 {
-    class SadrzajFajla
+    public class SadrzajFajla
     {
+
+        private int idfajla;
+
+        public int Idfajla
+        {
+            get { return idfajla; }
+            set { idfajla = value; }
+        }
+
+        private string Idsadrzaja;
+
+        public string IdSadrzaja
+        {
+            get { return Idsadrzaja; }
+            set { Idsadrzaja = value; }
+        }
+
+        private string sadrzaj;
+
+        public string Sadrzaj
+        {
+            get { return sadrzaj; }
+            set { sadrzaj = value; }
+        }
+
+        public SadrzajFajla()
+        {
+        }
+
+        public SadrzajFajla(int idfajla, string idsadrzaja, string sadrzaj)
+        {
+            if(idfajla < 0)
+            {
+                throw new ArgumentException("Id mora biti pozitivan broj veci od 0!");
+            }
+
+            this.idfajla = idfajla;
+            Idsadrzaja = idsadrzaja;
+            this.sadrzaj = sadrzaj;
+        }
     }
 }
