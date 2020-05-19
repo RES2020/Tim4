c1da654cc21bdaca84315d4fa515972ad82e0eff
Jovan275 <jovanconti86@gmail.com>
2020-05-19 14:23:46 +0200

    Zavrsetak drugog taska(jos uvek nije zavrsen)

---
+++ ResProjekat/Parser/PrimljeniTekst.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProsledjivanjeTeksta;
+using UnosTeksta;
+
+namespace Parser
+{
+  
+
+    public class PrimljeniTekst
+    {
+      
+
+        public static string poruka = "";
+
+       
+
+    }
+}
--- ResProjekat/UnosTeksta/Program.cs
+++ ResProjekat/UnosTeksta/Program.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnosTeksta;
+using ProsledjivanjeTeksta;
 
 namespace UnosTeksta
 {
    public class Program
     {
+        public static ProslediTekst pt = new ProslediTekst();
+
         static void Main(string[] args)
         {
             while (true)
             {
-                UnesiteTekst ut = new UnesiteTekst();
-                ut.Unos();
+                UnesiteTekst ut = new UnesiteTekst();             
+                pt.UnetiTekst = ut.Unos();
                 Console.ReadLine();
             }
         }
