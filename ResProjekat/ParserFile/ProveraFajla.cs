0c38642644f9cf6944979daade9fd2384d6e08e1
Milenko98 <61869042+Milenko98@users.noreply.github.com>
2020-06-11 13:46:42 +0200

    Uredjeni ispisi i provera fajla

--- ResProjekat/ParserFile/ProveraFajla.cs
+++ ResProjekat/ParserFile/ProveraFajla.cs
@@ -54,7 +54,7 @@ namespace ParserFile
                     else
                     {
                         b = false;
-                        break;
+                        //break;
                     }
                 }
             }
